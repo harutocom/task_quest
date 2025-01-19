a9e350d0728b9c3f058e1d6e1a96afc3c4284e07
harutocom <h.kobayashi.24.7.1536@4nonome.com>
2025-01-19 20:00:40 +0900

    タスクを入力して追加

--- task-quest/src/App.jsx
+++ task-quest/src/App.jsx
@@ -3,18 +3,37 @@ import TaskAdd from './components/taskAdd/taskAdd'
 import './App.css'
 
 
-function App() {
-  const [c,p] = useState(0);
-  const a = () =>{
-    p(c + 1);
-  }
+function App(){
+  const [isInputVisible,setIsInputVisible] = useState(false);  //texstarea表示/非表示
+  const [taskInput,setTaskInput] = useState("");  //入力されたタスクの内容
+  const [tasks,setTasks] = useState([]);  //タスク一覧
 
-  return (
-    <div>
-      <h1>{c}回</h1>
-      <TaskAdd onClick={a}></TaskAdd>
+  const addTask = () => {
+    if(taskInput.trim() != "") {
+      setTasks([...tasks,taskInput]);
+      setTaskInput("");
+    }
+  }
+  return(
+    <div style={{padding: "20px"}}>
+      <h1>タスクエ</h1>
+      {/* テキストエリアを表示するボタン */}
+      <TaskAdd onClick={() => setIsInputVisible(true)}>QEST作成</TaskAdd>
+      {/* isInputVisibleがtrueの時テキストエリア表示 */}
+      {isInputVisible && (
+        <div>
+          <input type="text" value={taskInput} 
+          onChange={(e) => setTaskInput(e.target.value)}
+          placeholder='クエスト内容を入力して'/>
+          <TaskAdd onClick={addTask}>QEST追加</TaskAdd>
+        </div>
+      )}
+      <h1>QEST一覧</h1>
+      <ul>
+        {tasks.map((task, index) => (<li key={index}>{task}</li>))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
